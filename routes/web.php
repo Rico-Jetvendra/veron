2644e97c0e9debb4133324b4ca42ff7848015e85
Rico-Jetvendra <rico.jetvendra@gmail.com>
2025-05-19 14:07:52 +0700

    Trying route link href

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->name('home');
 
 Route::get('/contact', function () {
     return view('pages/contact/index');
-});
+})->name('contact');
